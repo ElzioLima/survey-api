7e1a72400b71d36174099f2667d8e7ac020e6222
Elzio Nogueira Lima <elziolimach@gmail.com>
2023-01-06 23:24:32 -0400

    feat: add survey module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { SurveyModule } from './survey/survey.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, SurveyModule],
   controllers: [AppController],
   providers: [AppService],
 })
